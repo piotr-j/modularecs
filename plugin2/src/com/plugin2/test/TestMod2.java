d91e13ecb127fc4e073c372e2e56a0ecc5b53ed7
PiotrJ <me@piotrjastrzebski.io>
2015-09-24 20:28:10 +0200

    added another plugin

--- plugin2/src/com/plugin2/test/TestMod2.java
+++ plugin2/src/com/plugin2/test/TestMod2.java
@@ -1,4 +1,4 @@
-package com.plugin.test;
+package com.plugin2.test;
 
 import com.artemis.WorldConfiguration;
 import com.badlogic.gdx.Gdx;
@@ -8,11 +8,11 @@ import net.mountainblade.modular.annotations.Implementation;
 /**
  * Created by PiotrJ on 24/09/15.
  */
-@Implementation(authors = "PiotrJ", version = "0.1")
-public class TestMod implements GameMod {
-	private final static String TAG = TestMod.class.getSimpleName();
+@Implementation(authors = "PiotrJ", version = "0.12")
+public class TestMod2 implements GameMod {
+	private final static String TAG = TestMod2.class.getSimpleName();
 
-	public TestMod () {
+	public TestMod2 () {
 
 	}
 
