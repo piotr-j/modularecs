429a50562d8f5b0f6e2bb6a3c7f34d1928f76697
EvilEntity <me@piotrjastrzebski.io>
2015-09-26 23:40:36 +0200

    added asset for turret

--- plugin/src/com/plugin/test/processors/TurretSpawner.java
+++ plugin/src/com/plugin/test/processors/TurretSpawner.java
@@ -2,10 +2,14 @@ package com.plugin.test.processors;
 
 import com.artemis.*;
 import com.artemis.annotations.Wire;
+import com.badlogic.gdx.Gdx;
 import com.plugin.test.components.Turret;
 import com.plugin.test.components.TurretRef;
 import io.piotrjastrzebski.modularecs.plugin.api.Constants;
 import io.piotrjastrzebski.modularecs.plugin.api.components.*;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by PiotrJ on 25/09/15.
@@ -25,10 +29,13 @@ public class TurretSpawner extends EntitySystem {
 		Entity turret = world.createEntity();
 		EntityEdit edit = turret.edit();
 		edit.create(Transform.class).set(transform);
-		edit.create(Bounds.class).set(0.5f, 3f);
+		// TODO decouple bounds with asset size
+		edit.create(Bounds.class).set(48 * Constants.INV_SCALE, 140 * Constants.INV_SCALE);
 		// hardcoded, not great. would be nice if base had attachment points or something
 		edit.create(Turret.class).parent(entityId).offset(0, -24 * Constants.INV_SCALE);
 		edit.create(DebugTint.class).setColor(0, 1, 0);
+
+		edit.create(AssetRef.class).setPath("turret.png");
 
 		world.getEntity(entityId).edit().create(TurretRef.class).ref(turret.id);
 	}
