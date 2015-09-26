18756498c1e1723558b5962cc45cf25513bc0fe4
EvilEntity <me@piotrjastrzebski.io>
2015-09-26 23:41:14 +0200

    load files from plugin jars

--- core/src/io/piotrjastrzebski/modularecs/game/processors/Assets.java
+++ core/src/io/piotrjastrzebski/modularecs/game/processors/Assets.java
@@ -5,13 +5,21 @@ import com.artemis.ComponentMapper;
 import com.artemis.EntityEdit;
 import com.artemis.EntitySystem;
 import com.artemis.annotations.Wire;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.ObjectMap;
-import io.piotrjastrzebski.modularecs.plugin.api.Constants;
+import com.badlogic.gdx.utils.StreamUtils;
 import io.piotrjastrzebski.modularecs.plugin.api.components.Asset;
 import io.piotrjastrzebski.modularecs.plugin.api.components.AssetRef;
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * Created by EvilEntity on 26/09/2015.
@@ -25,7 +33,8 @@ public class Assets extends EntitySystem {
 	public Assets () {
 		super(Aspect.all(AssetRef.class));
 		setPassive(true);
-		manager = new AssetManager();
+		// todo make dist work
+		manager = new AssetManager(new JarHandleResolver("plugins/"));
 		textures = new ObjectMap<>();
 	}
 
@@ -53,5 +62,60 @@ public class Assets extends EntitySystem {
 	@Override protected void dispose () {
 		manager.dispose();
 		textures.clear();
+	}
+
+	private static class JarHandleResolver implements FileHandleResolver {
+		private InternalFileHandleResolver internal;
+		FileHandle jars;
+		public JarHandleResolver (String dir) {
+			jars = Gdx.files.internal(dir);
+			internal = new InternalFileHandleResolver();
+		}
+
+		@Override public FileHandle resolve (String fileName) {
+			FileHandle iHandle = internal.resolve(fileName);
+			if (iHandle.exists()) {
+				return iHandle;
+			}
+
+			// look in jars
+			// TODO cache this
+			for (FileHandle handle:jars.list()) {
+				if (!handle.extension().equals("jar")) continue;
+				FileHandle found = find(fileName, handle);
+				if (found != null) return found;
+			}
+			return null;
+		}
+
+		private FileHandle find (String fileName, FileHandle jar) {
+			try {
+				ZipInputStream zin = new ZipInputStream(jar.read());
+				ZipEntry entry;
+				String name;
+				while ((entry = zin.getNextEntry()) != null) {
+					name = entry.getName();
+					if (!name.endsWith(fileName)) continue;
+
+					BufferedOutputStream os = null;
+					try {
+						File tmp = File.createTempFile(fileName, null);
+						os = new BufferedOutputStream(new FileOutputStream(tmp));
+						StreamUtils.copyStream(zin, os);
+						return Gdx.files.absolute(tmp.getAbsolutePath());
+					} catch (FileNotFoundException ex) {
+						Gdx.app.error("Oops", "Unpack failed " + name);
+					} finally {
+						if (os != null) {
+							os.close();
+						}
+					}
+				}
+				zin.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			return null;
+		}
 	}
 }
